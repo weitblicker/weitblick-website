8f34224e4fa5de052cf7d13198bc0b39ac45fa58
Sebastian Pütz <spuetz@uos.de>
2018-12-05 15:46:49 +1300

    new template blog

--- wbcore/views.py
+++ wbcore/views.py
@@ -236,8 +236,7 @@ def blog_view(request, host_slug=None):
     except Host.DoesNotExist:
         raise Http404()
 
-    posts = posts.order_by('-published')
-    posts = reversed(posts)
+    posts = posts.order_by('-published')[:20]
 
     if host:
         breadcrumb = [('Home', reverse('home')), (host.name, reverse('host', args=[host_slug])), ("Blog", None)]
@@ -305,7 +304,7 @@ def news_view(request, host_slug=None):
         raise Http404()
 
     posts = posts.order_by('-published')[:20]
-    
+
     if host:
         breadcrumb = [('Home', reverse('home')), (host.name, reverse('host', args=[host_slug])), ("News", None)]
     else:
