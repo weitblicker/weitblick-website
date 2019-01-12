0e1c3d3d0d9ec92ea5b304dd4d32c12caf5745de
Sebastian Pütz <spuetz@uos.de>
2019-01-12 19:22:34 +0100

    fix sql join

--- data/read_blog.py
+++ data/read_blog.py
@@ -48,7 +48,7 @@ ssh_user = 'spuetz'
 ssh_port = 22
 ssh_pw = pw
 
-query = '''select p.ID, p.post_title as title, p.post_excerpt as excerpt, p.post_content as content, p.post_date as 'date', p.post_date_gmt as date_gmt, u.user_login, u.display_name, u.ID as user_id, u.user_email, t.name as country, GROUP_CONCAT(p2.Tag) as tags, GROUP_CONCAT(DISTINCT CONCAT(p3.guid,',% ', p3.post_date_gmt,',% ', p3.post_content) SEPARATOR ';%') as gallery, p.guid as old_page from wp_posts p 
+query = '''select p.ID, p.post_title as title, p.post_excerpt as excerpt, p.post_content as content, p.post_date as 'date', p.post_date_gmt as date_gmt, u.user_login, u.display_name, u.ID as user_id, u.user_email, t.name as country, GROUP_CONCAT(p2.Tag) as tags, GROUP_CONCAT(DISTINCT CONCAT(image_url,',% ', image_date,',% ', image_title) SEPARATOR ';%') as gallery, p.guid as old_page from wp_posts p 
 left join wp_users u on p.post_author=u.ID 
 inner join wp_term_relationships r on r.object_id=p.ID
 inner join wp_term_taxonomy tax on r.term_taxonomy_id=tax.term_taxonomy_id
@@ -56,8 +56,8 @@ inner join wp_terms t on tax.term_id=t.term_id
 left join (select p2.ID, t2.name as Tag from wp_posts p2 inner join wp_term_relationships r2 on r2.object_id=p2.ID
 inner join wp_term_taxonomy tax2 on r2.term_taxonomy_id=tax2.term_taxonomy_id
 inner join wp_terms t2 on tax2.term_id=t2.term_id where tax2.taxonomy = 'post_tag' ) p2 on p2.ID=p.ID
-left join (select p3.post_date_gmt, p3.post_content, p3.post_parent, p3.post_type, p3.guid from wp_posts p3) p3 on p3.post_parent=p.ID 
-where p.post_type='post' and tax.taxonomy = 'category' and p3.post_type = 'attachment' and
+left join (select i.post_date_gmt as image_date, i.post_content as image_title, i.post_parent, i.post_type, i.guid as image_url from wp_posts i where i.post_type = 'attachment') p3 on p3.post_parent=p.ID 
+where p.post_type='post' and tax.taxonomy = 'category' and
 t.name!='unsere Projekte' and p.post_status = 'publish' group by p.ID'''
 
 
