1290c74f6bdfad2696f61e3a54fa6cfc245163ba
Sebastian Pütz <spuetz@uos.de>
2019-10-07 21:01:50 +0200

    fix auth user by adding backend param

--- wbcore/views.py
+++ wbcore/views.py
@@ -547,7 +547,7 @@ def activate_user(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
