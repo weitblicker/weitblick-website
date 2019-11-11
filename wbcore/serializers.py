2229054714ab5e2442ce5ca34f54f3c6345d955a
Sebastian Pütz <spuetz@uos.de>
2019-11-11 12:20:27 +0100

    add gallery serializer

--- wbcore/serializers.py
+++ wbcore/serializers.py
@@ -1,19 +1,44 @@
 from wbcore.models import NewsPost, BlogPost, Host, Event, Project, Location
 from rest_framework import serializers
+from photologue.models import Gallery, Photo
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ('id', 'title', 'caption', 'image', 'date_taken', 'crop_from')
+
+
+class GallerySerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, gallery):
+        qs = gallery.photos.filter(is_public=True)
+        serializer = PhotoSerializer(instance=qs, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Gallery
+        depth = 1
+        fields = ('id', 'title', 'description', 'images')
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='pk')
+    gallery = GallerySerializer(read_only=True)
+
     class Meta:
         model = BlogPost
-        fields = ('id', 'title', 'text', 'image', 'added', 'updated', 'published', 'range', 'teaser')
+        fields = ('id', 'title', 'text', 'image', 'added', 'updated', 'published', 'range', 'teaser', 'gallery')
 
 
 class NewsPostSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='pk')
+    gallery = GallerySerializer(read_only=True)
+
     class Meta:
         model = NewsPost
-        fields = ('id', 'title', 'text', 'image', 'added', 'updated', 'published', 'range', 'teaser')
+        fields = ('id', 'title', 'text', 'image', 'added', 'updated', 'published', 'range', 'teaser', 'gallery')
 
 
 class HostSerializer(serializers.ModelSerializer):
@@ -25,20 +50,27 @@ class HostSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='pk')
+    gallery = GallerySerializer(read_only=True)
+
     class Meta:
         model = Project
+        depth = 0
         fields = ('id', 'start_date', 'end_date', 'published', 'name', 'slug', 'hosts', 'description', 'location',
-                  'partner',)
+                  'partner', 'gallery')
+
 
 class EventSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='pk')
+    gallery = GallerySerializer(read_only=True)
+
     class Meta:
         model = Event
-        fields = ('id', 'name', 'projects', 'host')
+        fields = ('id', 'name', 'projects', 'host', 'gallery')
 
 
 class LocationSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='pk')
+
     class Meta:
         model = Location
         fields = ('id', 'name', 'description', 'country', 'postal_code', 'city', 'state', 'street', 'address', 'lat', 'lng')
