bd03a4511e7cf6fde5f5ed20b400fb9fd0bb043a
Sebastian Pütz <spuetz@uos.de>
2019-11-26 18:23:02 +0100

    markdown extension for ui content tabs

--- wbcore/markdown/content_tabs.py
+++ wbcore/markdown/content_tabs.py
@@ -1,93 +1,72 @@
 from markdown import util
+from markdown.blockprocessors import BlockProcessor
 from markdown.extensions import Extension
 from markdown.inlinepatterns import SimpleTagPattern, ImageInlineProcessor, LinkInlineProcessor, IMAGE_LINK_RE
-from markdown.treeprocessors import Treeprocessor
+import re
 
 
-class ImageInlineProcessor(LinkInlineProcessor):
-    """ Return a img element from the given match. """
+class ContentTabProcessor(BlockProcessor):
 
-    def handleMatch(self, m, data):
-        text, index, handled = self.getText(data, m.end(0))
-        if not handled:
-            return None, None, None
+    RE = re.compile(r'# \[(.*?)\]\(#tab/([a-zA-Z0-9\-]+)(?:/([a-zA-Z0-9\-]+)?)?\)')
+    RE_BREAK = re.compile(r'(.*?)(---)(.*?)')
 
-        src, title, index, handled = self.getLink(data, index)
-        if not handled:
-            return None, None, None
+    def test(self, parent, block):
+        se = self.RE.search(block)
+        #print("re:", se)
+        return bool(se)
 
-        sizes = ['mini', 'tiny', 'small', 'medium', 'large', 'big', 'huge']
-        size = "medium"
+    def run(self, parent, blocks):
+        collection = []
 
-        for s in sizes:
-            if text.endswith(s):
-                text = text.replace(s, "")
-                size = s
+        container = util.etree.Element("ui container")
+        parent.append(container)
+        menu = util.etree.Element("div")
+        menu.set("class", "ui top attached tabular menu")
+        container.append(menu)
+
+        first = None
+
+        while blocks:
+            block = blocks.pop(0)
+            m = self.RE.search(block)
+            print(m, block)
+            if m:
+                title = m.groups()[0]
+                id = m.groups()[1]
+                block = self.RE.sub("", block)
+                #print("Block id:", id, "title:", title, "content:", block)
+                button = util.etree.Element("a")
+                button.set("class", "item")
+                button.set("data-tab", id)
+                button.text = title
+                menu.append(button)
+
+                content_div = util.etree.Element("div")
+                content_div.set("class", "ui bottom attached tab segment")
+                content_div.set("data-tab", id)
+                container.append(content_div)
+
+                if not first:
+                    first = True
+                    button.set("class", button.get("class") + " active")
+                    content_div.set("class", content_div.get("class") + " active")
+
+                b = self.RE_BREAK.search(block)
+                if b:
+                    print("BB", b)
+                    block = b.groups()[0]
+                    self.parser.parseChunk(content_div, block)
+                    next_block = b.groups()[2]
+                    self.parser.parseChunk(parent, next_block)
+                    break
+                else:
+                    self.parser.parseChunk(content_div, block)
+            else:
                 break
-
-        side = ""
-        ui_class = "ui rounded fluid image"
-        if text.endswith("<>"):
-            ui_class = "ui rounded centered %s image" % (size,)
-            side = "centered"
-        elif text.endswith("<"):
-            ui_class = "ui rounded left floated %s image" % (size, )
-            side = "left floated"
-        elif text.endswith(">"):
-            side = "right floated"
-            ui_class = "ui rounded right floated %s image" % (size, )
-
-        # the outer div object for ui
-        div = util.etree.Element("div")
-        div.set("class", ui_class)
-
-        # img tag
-        img = util.etree.SubElement(div, "img")
-        img.set("src", src)
-        if side == 'centered':
-            img.set("class", "ui centered image")
-        if title is not None:
-            img.set("title", title)
-
-        img.set('alt', self.unescape(text))
-
-        # if title available show caption div
-        if title:
-            caption = util.etree.SubElement(div, "div")
-            caption.set("class", "ui bottom attached label")
-            caption.text = title
-
-        return div, m.start(0), index
-
-
-class ImageClearTreeprocessor(Treeprocessor):
-
-    def findImgDiv(self, elem):
-        for index in range(0, len(elem)):
-            class_attr = elem[index].get('class')
-            if class_attr and "image" in class_attr and "ui" in class_attr:
-                return index
-            i = self.findImgDiv(elem[index])
-            if i is not None:
-                clear = util.etree.Element("div")
-                clear.set("style", "clear:both;")
-                elem[index].insert(i, clear)
-
-        return None
-
-    def run(self, root):
-        """ Crawl the footnote div and add missing duplicate footnotes. """
-        self.findImgDiv(root)
-        return None
-                    #if div.get("class").contains("image"):
-                    #    print(index)
-
-
 
 class MyExtension(Extension):
     def extendMarkdown(self, md, md_globals):
-        md.inlinePatterns.register(ImageInlineProcessor(IMAGE_LINK_RE, md), 'image_link', 150)
-        md.treeprocessors.register(ImageClearTreeprocessor(), 'image_clear', 20)
+        md.parser.blockprocessors.register(ContentTabProcessor(md.parser), 'content_tabs', 200)
 
 
 def makeExtension(**kwargs):  # pragma: no cover
