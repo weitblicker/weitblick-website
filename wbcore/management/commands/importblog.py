e1c7b138c91c3db514d6da19575d746cc6a49cd8
Sebastian Pütz <spuetz@uos.de>
2019-11-29 19:14:44 +0100

    add import blog command

--- wbcore/management/commands/importblog.py
+++ wbcore/management/commands/importblog.py
@@ -1,5 +1,9 @@
+import getpass
+
+import pandas as pd
+import pymysql
 from django.core.management.base import BaseCommand, CommandError
-from wbcore.models import BlogPost, Host, Photo, NewsPost
+from wbcore.models import BlogPost, Host, Photo
 import re
 import json
 import sys
@@ -7,12 +11,41 @@ import urllib.request
 from slugify import slugify
 import os
 import datetime
+from sshtunnel import SSHTunnelForwarder
 from urllib import request, error
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 
+
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
+
+    sql_hostname = 'localhost'
+    sql_username = 'root'
+    sql_password = None
+    sql_main_database = 'weitweg-blog'
+    sql_port = 3306
+
+    ssh_host = 'weitblicker.org'
+    ssh_user = 'spuetz'
+    ssh_port = 22
+    ssh_pw = None
+
+    df = None
+
+    query = '''select p.ID, p.post_title as title, p.post_excerpt as excerpt, p.post_content as content, p.post_date as 'date', p.post_date_gmt as date_gmt, u.user_login, u.display_name, u.ID as user_id, u.user_email, t.name as country, GROUP_CONCAT(p2.Tag) as tags, GROUP_CONCAT(DISTINCT CONCAT(image_url,',% ', image_date,',% ', image_title) SEPARATOR ';%') as gallery, p.guid as old_page from wp_posts p 
+    left join wp_users u on p.post_author=u.ID 
+    inner join wp_term_relationships r on r.object_id=p.ID
+    inner join wp_term_taxonomy tax on r.term_taxonomy_id=tax.term_taxonomy_id
+    inner join wp_terms t on tax.term_id=t.term_id
+    left join (select p2.ID, t2.name as Tag from wp_posts p2 inner join wp_term_relationships r2 on r2.object_id=p2.ID
+    inner join wp_term_taxonomy tax2 on r2.term_taxonomy_id=tax2.term_taxonomy_id
+    inner join wp_terms t2 on tax2.term_id=t2.term_id where tax2.taxonomy = 'post_tag' ) p2 on p2.ID=p.ID
+    left join (select i.post_date_gmt as image_date, i.post_content as image_title, i.post_parent, i.post_type, i.guid as image_url from wp_posts i where i.post_type = 'attachment') p3 on p3.post_parent=p.ID 
+    where p.post_type='post' and tax.taxonomy = 'category' and
+    t.name!='unsere Projekte' and p.post_status = 'publish' group by p.ID'''
+
+
 
     oldest = datetime_object = datetime.datetime.strptime('2008-01-01', '%Y-%m-%d')
     skip_citys = ['tuebingen']
@@ -36,25 +69,28 @@ class Command(BaseCommand):
 
     slugs = {}
     titles = {}
-    users = {}
 
     def add_arguments(self, parser):
-        parser.add_argument('news-file', nargs=1, type=str)
-        parser.add_argument('user-file', nargs=1, type=str)
+        pass
 
     def handle(self, *args, **options):
-        news_file = options['news-file']
-        user_file = options['user-file']
 
-        print(news_file, user_file)
+        pw = getpass.getpass()
+        self.ssh_pw = pw
+        self.sql_password = pw
+        #self.ssh_user = getpass.getuser()
 
-        with open(user_file[0]) as f:
-            self.user_data = json.load(f)
-            self.users = Command.parse_user_data(self.user_data)
+        with SSHTunnelForwarder(
+                (self.ssh_host, self.ssh_port),
+                ssh_username=self.ssh_user,
+                ssh_password=self.ssh_pw,
+                remote_bind_address=(self.sql_hostname, self.sql_port)) as tunnel:
+            conn = pymysql.connect(host='127.0.0.1', user=self.sql_username, passwd=self.sql_password,
+                                   db=self.sql_main_database, port=tunnel.local_bind_port)
+            df = pd.read_sql_query(self.query, conn)
+            conn.close()
 
-        with open(news_file[0]) as f:
-            self.news_data = json.load(f)
-            self.parse_news_data(self.news_data)
+            self.parse_news_data(df)
 
     @staticmethod
     def umlaute(value):
@@ -62,21 +98,6 @@ class Command(BaseCommand):
         for (s, r) in replacements:
             value = value.replace(s, r)
         return value
-
-    @staticmethod
-    def parse_user_data(user_data):
-        users = {}
-        for user in user_data['users']:
-
-            user = user['user']
-            name = user['name']
-            mail = user['E-Mail']
-            uid = user['uid']
-            users[uid] = {
-                'name': name,
-                'mail': mail
-            }
-        return users
 
     def make_slug(self, name):
         slug = slugify(self.umlaute(name.lower()))
@@ -96,41 +117,79 @@ class Command(BaseCommand):
 
         return name + '-' + str(self.titles[name])
 
-    def parse_news_data(self, news_data):
-        for article in news_data["news-list"]:
-            article = article['news-article']
+    def parse_news_data(self, df):
 
-            dt = datetime.datetime.strptime(article['datetime'], "%Y-%m-%d %H:%M:%S")
-            if dt < self.oldest:
-                continue
+        for index, article in df.iterrows():
 
+            ## read date time
+            dt = pd.to_datetime(article['date'], format="%Y-%m-%d %H:%M:%S")
             slug_date = dt.strftime("%Y-%m-%d")
+            #dt = dt_obj.tz_localize('UTC').strftime("%Y-%m-%d %H:%M:%S.%f")
 
-            host_slug = article['wb-host'].lower()
-            host_slug = Command.umlaute(host_slug)
-            if host_slug in self.skip_citys:
-                continue
+            ## read host
+            host = 'muenster'
 
-            user_id = article['Autor']
-            user = self.users[user_id]['name']
-            host = Host.objects.get(slug=host_slug)
-            title = article['article-title'].strip()
-            text = article['article-text'].strip()
-            teaser = article['conclusion'].strip()
-            image = article['teaserimage']['src'].strip()
-            slug = slug_date+'-'+slugify(self.umlaute(title.lower()))
+            ## read user
+            user = article['user_login']
+
+            ## read content
+            text = article['content']
+
+            ## read title
+            title = article['title']
+            if not isinstance(title, str):
+                title = title[0]
+            title = title.strip()
 
             if not title:
                 print("############## TITLE is not set... continue with next blog entry...")
                 continue
 
+            ## read teaser
+            teaser = article['excerpt']
+
+            ##
+            slug = self.make_slug(title)
+
+            ## read gallery
+
+            gallery_raw = article['gallery']
+            gallery = []
+            if gallery_raw is not None:
+                gallery_raw = gallery_raw.split(';%')
+                for entry in gallery_raw:
+                    try:
+                        g_photo_url, g_photo_date, g_photo_title = entry.split(',%')
+                        g_photo_date = pd.to_datetime(g_photo_date, format="%Y-%m-%d %H:%M:%S")
+                        gallery.append({
+                            'url': g_photo_url,
+                            'date': g_photo_date,
+                            'title': g_photo_title,
+                        })
+                    except:
+                        g_photo_entry = entry.split(',%')
+                        if len(g_photo_entry) > 0:
+                            for ext in self.image_extensions:
+                                if g_photo_entry[0].endswith(ext):
+                                    gallery.append({
+                                        'url': g_photo_entry[0],
+                                        'date': dt,
+                                        'title': "",
+                                    })
+                                    break
+                        else:
+                            print("Exception: Gallery string does not consists out of three values:", entry.split(',%'))
+
+
+            tags = self.caption_img.findall(text)
+
             try:
-                post = NewsPost.objects.get(slug=slug)
+                post = BlogPost.objects.get(slug=slug)
                 continue
-            except:
-                post = NewsPost(title=title, slug=slug, text=text, text_de=text, added=dt,
+            except BlogPost.DoesNotExist:
+                post = BlogPost(title=title, slug=slug, text=text, text_de=text, added=dt,
                             updated=dt, published=dt, range='global', teaser=teaser,
-                            teaser_de=teaser, host=host, author_str=user)
+                            teaser_de=teaser, host=Host.objects.get(slug=host), author_str=user)
             post.save()
 
             def sub_image_match(match):
@@ -144,9 +203,11 @@ class Command(BaseCommand):
                     return '![%s](%s "%s")' % (photo.title, photo.image.url, photo.title)
                 return ""  # remove from html and just add link to photologue object
 
-            photo = self.get_photo(image, dt, title)
-            if photo:
-                post.photos.add(photo)  # add teaser image
+            for elem in gallery:
+                img_title = elem['title'] if elem['title'] else title # if image has title use it, else use article title
+                photo = self.get_photo(title=img_title, date=elem['date'], image=elem['url'])
+                if photo:
+                    post.photos.add(photo)
 
             text = self.caption_img.sub(sub_image_match, text)
             text = self.img_pat.sub(sub_image_match, text)
@@ -184,7 +245,7 @@ class Command(BaseCommand):
             try:
                 photo = Photo.objects.get(title=title, slug=slug)
             except:
-                photo = Photo(title=title, slug=slug, caption=caption, date_added=date, type='news')
+                photo = Photo(title=title, slug=slug, caption=caption, date_added=date, type='blog')
                 photo.image.save(new_name, File(img_temp))
                 photo.save()
             return photo
