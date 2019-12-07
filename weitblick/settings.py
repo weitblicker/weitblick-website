b415ce3c4c7e3941cd0abeeecd5b07a1fe560a51
Sebastian Pütz <spuetz@uos.de>
2019-12-07 13:23:47 +0100

    fix auth order

--- weitblick/settings.py
+++ weitblick/settings.py
@@ -284,8 +284,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
